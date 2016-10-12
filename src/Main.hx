8f8a6ac551b3c9c0918626038d6491094837a6bc
Marcin Kotz <marcin.kotz@ives-system.com>
2016-10-12 08:52:42 +0200

    initial test

---
+++ src/Main.hx
@@ -0,0 +1,11 @@
+package;
+
+import com.er453r.TestClass;
+
+class Main{
+	public static function main() {
+		var test:TestClass = new TestClass();
+
+		trace(test.publicVar);
+	}
+}
---
+++ src/com/er453r/TestClass.hx
@@ -0,0 +1,11 @@
+package com.er453r;
+
+class TestClass {
+	private var privateVar:Float;
+	public var publicVar:Float;
+
+	public function new() {
+		publicVar = 0.5;
+		privateVar = 3.4;
+	}
+}
