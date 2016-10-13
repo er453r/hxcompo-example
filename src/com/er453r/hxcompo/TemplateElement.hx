29d2e08748945f442a986aa1440a85ce84348b5a
Marcin Kotz <marcin.kotz@ives-system.com>
2016-10-13 10:23:21 +0200

    setup as example project

--- src/com/er453r/hxcompo/TemplateElement.hx
+++
@@ -1,9 +0,0 @@
-package com.er453r.hxcompo;
-
-import js.html.Element;
-import js.html.Node;
-
-@:native("HTMLTemplateElement")
-extern class TemplateElement extends Element{
-	public var content:Node;
-}
